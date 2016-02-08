bfe651bf116c3869213759eb9f0cdd8a9c219b19
Jose <joselmacs20@gmail.com>
2016-02-08 05:36:33 +0100

    Original

---
+++ Evaluacion_U2/src/paquete/com/utng/asociaciones/Rubro.java
@@ -0,0 +1,25 @@
+
+
+package paquete.com.utng.asociaciones;
+
+
+/***
+ * 
+ * @author Jose
+ *
+ */
+public class Rubro {
+	/**
+	 * Atributos de la clase
+	 */
+	private String rb_descripcion;
+	private int rb_ID;
+
+	/**
+	 * constructor de la clase
+	 */
+	public Rubro() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
